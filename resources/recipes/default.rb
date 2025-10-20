7b63868aeeddc64cffd692fa1d1c9b9135f8b553
Rafael Gomez <rgomez@redborder.com>
2025-10-20 12:18:24 +0100

    Fix providers name configuration

--- resources/recipes/default.rb
+++ resources/recipes/default.rb
@@ -3,10 +3,10 @@
 # Copyright:: 2024, redborder
 # License:: Affero General Public License, Version 3
 
-airflow_scheduler_config 'Configure Airflow Scheduler' do
+airflow_scheduler 'Configure Airflow Scheduler' do
   action :add
 end
 
-airflow_webserver_config 'Configure Airflow Webserver' do
+airflow_webserver 'Configure Airflow Webserver' do
   action :add
 end
